1cd10dc59a0a41f694142013d2fc58158fbfd92b
SCHcoding <codingb007@gmail.com>
2024-09-20 17:20:35 +0900

    디자인

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function App() {
   return (
     <div className="App">
       <h1>TODO LIST</h1>
+      <Form>
+        <Form.Group
+          className="mb-3 d-flex align-items-center gap-3"
+          controlId="exampleForm.ControlInput1"
+        >
+          <Form.Label>TODO Input</Form.Label>
+          <Form.Control type="text" placeholder="할 일을 입력하세요" />
+        </Form.Group>
+      </Form>
+      <hr></hr>
     </div>
   );
 }
