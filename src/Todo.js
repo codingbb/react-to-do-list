2dcead023c453d67658a7f8bd859dc35a547e0db
SCHcoding <codingb007@gmail.com>
2024-09-23 00:28:14 +0900

    코드 수정

--- src/Todo.js
+++ src/Todo.js
@@ -3,7 +3,13 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 
 const Todo = ({ data, deleteTodo, setChecked }) => {
+  const [mode, setMode] = useState("read");
+  const [text, setText] = useState(data.text);
   const [isChecked, setIsChecked] = useState(false);
+
+  let className = "d-flex gap-2";
+  let formClass = "hidden";
+
   const handleCheckbox = () => {
     setIsChecked(!isChecked);
     setChecked(data.id, !isChecked);
@@ -14,9 +20,14 @@ const Todo = ({ data, deleteTodo, setChecked }) => {
     deleteTodo(data.id);
   };
 
+  if (mode === "edit") {
+    className += " hidden";
+    formClass = "";
+  }
+
   return (
     <div>
-      <Form.Check id={`check-api-${data.id}`}>
+      <Form.Check className={className}>
         <Form.Check.Input
           type="checkbox"
           id={`check-api-${data.id}`}
@@ -26,7 +37,33 @@ const Todo = ({ data, deleteTodo, setChecked }) => {
         <Button variant="danger" size="sm" onClick={todoDelete}>
           Delete
         </Button>
+        <Button
+          variant="info"
+          size="sm"
+          onClick={() => {
+            setMode("edit");
+          }}
+        >
+          Edit
+        </Button>
       </Form.Check>
+      <Form className={formClass}>
+        <Form.Group
+          className="mb-3 d-flex gap-2"
+          controlId={`edit-todo-${data.id}`}
+        >
+          <Form.Control type="text" value={text} />
+          <Button variant="secondary">Update</Button>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              setMode("read");
+            }}
+          >
+            cancel
+          </Button>
+        </Form.Group>
+      </Form>
     </div>
   );
 };
